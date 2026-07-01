91ea9c36813bc9cd0f0c346e57bf2aa1cffd8d2c
Mikael Torres <mikaelt76@gmail.com>
2026-07-01 20:27:26 -0300

    fix: correção background página destination tablet/desktop

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -12,8 +12,8 @@ function Destination() {
     return (
         <div className=" w-full h-screen bg-cover bg-center 
         bg-[url('/src/assets/destination/background-destination-mobile.jpg')]
-        md:bg-[url('src/assets/destination/background-destination-tablet.jpg')]
-        lg:bg-[url('src/assets/destination/background-destination-desktop.jpg')]
+        md:bg-[url('/src/assets/destination/background-destination-tablet.jpg')]
+        lg:bg-[url('/src/assets/destination/background-destination-desktop.jpg')]
         "
         >
 
